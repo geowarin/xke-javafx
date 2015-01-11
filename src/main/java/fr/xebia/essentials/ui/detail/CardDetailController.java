efefbfaa223207274a48bf16ddea2cecee5ed338
Gautier Nilhcem <nilhcem@gmail.com>
2015-01-11 18:30:44 +0100

    Start creating second screen

---
+++ src/main/java/fr/xebia/essentials/ui/detail/CardDetailController.java
@@ -0,0 +1,13 @@
+package fr.xebia.essentials.ui.detail;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CardDetailController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+}
