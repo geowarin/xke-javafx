f63fb432e0e9c44ecfe62be902c23f1d23aeac3f
Gautier Nilhcem <nilhcem@gmail.com>
2015-01-11 16:34:02 +0100

    CardsListItem is now a controller and binds data automatically

--- src/main/java/fr/xebia/essentials/ui/list/CardsListItem.java
+++ src/main/java/fr/xebia/essentials/ui/list/CardsListItem.java
@@ -1,20 +1,39 @@
 package fr.xebia.essentials.ui.list;
 
 import fr.xebia.essentials.model.Card;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 
+import java.io.IOException;
+
 public class CardsListItem extends ListCell<Card> {
+
+    private Parent root;
+    @FXML Label cardTitle;
+
+    public CardsListItem() {
+        super();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/cards_list_item.fxml"));
+        fxmlLoader.setController(this);
+        try {
+            root = fxmlLoader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
     protected void updateItem(Card item, boolean empty) {
         super.updateItem(item, empty);
-        if (item == null) {
+        if (empty) {
             setText(null);
             setGraphic(null);
         } else {
-            Label label = new Label(item.getTitle());
-            setGraphic(label);
+            cardTitle.setText(item.getTitle());
+            setGraphic(root);
         }
     }
 }
