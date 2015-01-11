29e2d4884a1f8c8423cf18d2966ca75de6912565
Gautier Nilhcem <nilhcem@gmail.com>
2015-01-11 14:57:31 +0100

    Add first fxml

--- src/main/java/fr/xebia/essentials/Main.java
+++ src/main/java/fr/xebia/essentials/Main.java
@@ -1,12 +1,16 @@
 package fr.xebia.essentials;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -19,7 +23,7 @@ public class Main extends Application {
         initPrimaryStage(primaryStage);
     }
 
-    private void initPrimaryStage(Stage primaryStage) {
+    private void initPrimaryStage(Stage primaryStage) throws IOException {
         Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
         double height = bounds.getHeight() * 0.8;
         double width = height * 0.7;
@@ -28,10 +32,8 @@ public class Main extends Application {
         primaryStage.setX(bounds.getMinX());
         primaryStage.setY(bounds.getMinY());
 
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/cards_list.fxml"));
         primaryStage.setTitle("Xebia Essentials");
-        Button button = new Button("Click me");
-        button.setOnAction(event -> System.out.println("Clicked!"));
-        StackPane root = new StackPane(button);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
