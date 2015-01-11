6b362590fb52d31188d45a12ff41559651a6e84a
Gautier Nilhcem <nilhcem@gmail.com>
2015-01-11 13:49:08 +0100

    Create button manually

--- src/main/java/fr/xebia/essentials/Main.java
+++ src/main/java/fr/xebia/essentials/Main.java
@@ -1,8 +1,28 @@
 package fr.xebia.essentials;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
     public static void main(String[] args) {
-        System.out.println("XKE");
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        initPrimaryStage(primaryStage);
+    }
+
+    private void initPrimaryStage(Stage primaryStage) {
+        primaryStage.setTitle("Xebia Essentials");
+        Button button = new Button("Click me");
+        button.setOnAction(event -> System.out.println("Clicked!"));
+        StackPane root = new StackPane(button);
+        primaryStage.setScene(new Scene(root, 320, 200));
+        primaryStage.show();
     }
 }
